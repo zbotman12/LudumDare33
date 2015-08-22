0b3bda8b269166b9daeb33889cbf988bfe5e5ad2
Ryan Kline <trk0002@uah.edu>
2015-08-22 18:19:53 -0500

    ciojeijsidas lighting

--- LudumDare33/Assets/Scripts/CharacterNavMesh.cs
+++ LudumDare33/Assets/Scripts/CharacterNavMesh.cs
@@ -18,23 +18,31 @@ public class CharacterNavMesh : MonoBehaviour {
 		GameObject text = GameObject.Find("Tooltip3d(Clone)");
 
 		if (Physics.Raycast (ray, out hit, 1000000)) {
-			if (Input.GetMouseButtonDown (0)) {	
-				agent.SetDestination (hit.point);
+
+			//DIALOG and WALKING
+			if (Input.GetMouseButtonDown (0)) {
+				if(hit.transform.tag == "NPC" || hit.transform.tag == "Interactable"){  
+					hit.transform.gameObject.SendMessage("Interact");
+				}else{
+					agent.SetDestination (hit.point);
+				}
 			}
-			if (hit.transform.tag == "NPC" && !isActive) {
-				isActive = true;
-				Instantiate(ToolText, new Vector3(hit.point.x,10,hit.point.z), Quaternion.identity);
-				//text.transform.localPosition = new Vector3(hit.point.x,10,hit.point.z);
-			}else if (hit.transform.tag != "NPC"){
-				isActive = false;
-				Destroy(GameObject.Find("Tooltip3d(Clone)"));
-				text = null;
-			}
-			if(text != null){
-			text.transform.localPosition = hit.point;//new Vector3(hit.point.x,10,hit.point.z);
-			}
-			text.GetComponent<TextMesh>().text = hit.transform.gameObject.name;
-			text.GetComponent<TextMesh>().fontSize = 60;
+			//TOOLTIPS
+				if (hit.transform.tag == "NPC" && !isActive) {
+					isActive = true;
+					Instantiate(ToolText, new Vector3(hit.point.x,10,hit.point.z), Quaternion.identity);
+					//text.transform.localPosition = new Vector3(hit.point.x,10,hit.point.z);
+				}else if (hit.transform.tag != "NPC"){
+					isActive = false;
+					Destroy(GameObject.Find("Tooltip3d(Clone)"));
+					text = null;
+				}
+				if(text != null){
+				text.transform.localPosition = hit.point;//new Vector3(hit.point.x,10,hit.point.z);
+				}
+				text.GetComponent<TextMesh>().text = hit.transform.gameObject.name;
+				text.GetComponent<TextMesh>().fontSize = 60;
+		
 		}
 	}
 }
