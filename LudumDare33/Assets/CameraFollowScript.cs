386db593981ae952253c542456fcace4d6109498
Ryan Kline <trk0002@uah.edu>
2015-08-22 12:50:42 -0500

    day 1

---
+++ LudumDare33/Assets/CameraFollowScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	public GameObject Player;
+	private Vector3 offset;
+	//public Time delay;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Player.transform.position + offset;
+	}
+}
---
+++ LudumDare33/Assets/CharacterNavMesh.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterNavMesh : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, 1000000)){
+				agent.SetDestination(hit.point);
+			}
+		}
+
+	}
+}
